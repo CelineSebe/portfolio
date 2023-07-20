0748f11096e066d759f4b8a955495c5c14927c74
Céline Sèbe <celine.sebe@gmail.com>
2023-07-20 18:54:53 +0200

    rendered static

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.BASE_PATH
+  basePath: process.env.BASE_PATH,
+  images: {
+    unoptimized: true
+}
 }
 
 module.exports = nextConfig
